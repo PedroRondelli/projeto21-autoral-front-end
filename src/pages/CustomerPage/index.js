83cb41345a2833587aa721fb5a4c9317112eaaf9
Pedro Rondelli <pedrorondelli@hotmail.com>
2023-06-21 09:51:27 -0300

    feat: implements AuthForm on CustomerPage

--- src/pages/CustomerPage/index.js
+++ src/pages/CustomerPage/index.js
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import { Container } from "../../assets/Container";
 import { StudioName } from "../../assets/StudioName";
+import { ReadyButton } from "../../assets/ReadyButton";
+import { AuthForm } from "../../assets/AuthForm";
 
 export default function CustomerPage() {
   return (
     <Container>
       <StudioName className="letterPattern">RONDELLI TATTOO</StudioName>
-      <WelcomeMessage className="letterPattern" >SEJA BEM VINDO (a)!</WelcomeMessage>
-      <input></input>
-      <button></button>
+      <WelcomeMessage className="letterPattern">
+        SEJA BEM VINDO (a)!
+      </WelcomeMessage>
+      <AuthForm>
+        <input placeholder="SEU NOME"></input>
+        <ReadyButton>PRONTO</ReadyButton>
+      </AuthForm>
     </Container>
   );
 }
