5b607cf9b2bacb707db3873ed964604c80ee6b8e
Pedro Rondelli <pedrorondelli@hotmail.com>
2023-07-10 09:29:13 -0300

    style:remove styled component "Form" Edition Page

--- src/pages/ProfileEditionPage/index.js
+++ src/pages/ProfileEditionPage/index.js
@@ -81,45 +81,6 @@ const Container = styled.div`
   }
   box-sizing: border-box;
 `;
-const Form = styled.form`
-  width: 50vw;
-  height: 100vh;
-
-  padding: 20px;
-  box-sizing: border-box;
-
-  background-color: #444040;
-  box-shadow: 10px 4px 4px rgba(0, 0, 0, 0.25);
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  input {
-    border-radius: 32px;
-    height: 11vh;
-    padding: 10px;
-
-    box-sizing: border-box;
-
-    font-family: Saira Stencil One;
-    font-size: 2vw;
-    font-weight: 400;
-    line-height: 50px;
-    letter-spacing: 0em;
-  }
-  @media (max-width: 414px) {
-    width: 100vw;
-    height: 70vh;
-    justify-content: flex-start;
-    input {
-      font-size: 3vw;
-      margin: 10px auto;
-      height: 8vh;
-      width: 100%;
-    }
-  }
-`;
 const PhotoContainer = styled.div`
   width: 50vw;
   height: 100vh;
