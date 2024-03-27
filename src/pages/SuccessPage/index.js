6c7aeb2bd3474e41b02d7a685f551e86b824d2a7
PedroRondelli <pedrorondelli@hotmail.com>
2024-03-27 11:37:29 -0300

    fully implements the success page

--- src/pages/SuccessPage/index.js
+++ src/pages/SuccessPage/index.js
@@ -1,13 +1,31 @@
 import styled from "styled-components";
 
-export default function SuccessPage(){
-    return (<SuccessPageWrapper>
-        
-    </SuccessPageWrapper>)
+export default function SuccessPage() {
+  return (
+    <SuccessPageWrapper>
+      <SuccessIconBox>
+        <ion-icon name="chevron-down-circle-outline"></ion-icon>
+      </SuccessIconBox>
+      <SuccessText>Sessão marcada com sucesso ! !</SuccessText>
+    </SuccessPageWrapper>
+  );
 }
 
-
-const SuccessPageWrapper=styled.div`
-background-color: #008000;
-min-height: 100vh;
-`
+const SuccessPageWrapper = styled.div`
+  background-color: #008000;
+  min-height: 100vh;
+`;
+const SuccessIconBox = styled.div`
+  display: flex;
+  justify-content: center;
+  ion-icon {
+    color: white;
+    font-size: 500px;
+  }
+`;
+const SuccessText = styled.p`
+  color: #ffff;
+  font-family: "Saira Stencil One";
+  font-size: 3vw;
+  text-align: center;
+`;
