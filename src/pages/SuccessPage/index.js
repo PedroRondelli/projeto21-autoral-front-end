009e3ff521179910a6400b537c96736b80293bda
PedroRondelli <pedrorondelli@hotmail.com>
2024-03-27 11:17:52 -0300

    style: add background color and min-height to success page

--- src/pages/SuccessPage/index.js
+++ src/pages/SuccessPage/index.js
@@ -1,1 +1,13 @@
-export default function SuccessPage(){}
+import styled from "styled-components";
+
+export default function SuccessPage(){
+    return (<SuccessPageWrapper>
+        
+    </SuccessPageWrapper>)
+}
+
+
+const SuccessPageWrapper=styled.div`
+background-color: #008000;
+min-height: 100vh;
+`
